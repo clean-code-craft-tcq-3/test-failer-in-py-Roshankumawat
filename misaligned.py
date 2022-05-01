128ac3443773d39d73dec8cb51fc93ecdcf369b8
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-05-01 17:55:19 +0530

    Added test code to check validity of Pair Number

--- misaligned.py
+++ misaligned.py
@@ -4,9 +4,12 @@ def print_color_map():
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
+            checkValidPairNumber(i * 5 + j)
             print(f'{i * 5 + j} | {major} | {minor}')
     return len(major_colors) * len(minor_colors)
-
+def checkValidPairNumber(pair_number):
+    assert(pair_number<1)
+    assert(pair_number>25)
 
 result = print_color_map()
 assert(result == 25)
