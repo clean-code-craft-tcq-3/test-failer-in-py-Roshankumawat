0a562b726f78d5812370fe1029d483f3984058bb
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-05-06 15:53:13 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -1,15 +1,20 @@
+major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
+minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+
 def print_color_map():
-    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
             checkValidPairNumber(i*5+j+1)
-            print(f'{i * 5 + j : <2} | {major:<6} | {minor:<6}')
+            print( f'{i * 5 + j : <2} | {major:<6} | {minor:<6}')
+
+def get_total_pair_numbers(major_colors, minor_colors):
     return len(major_colors) * len(minor_colors)
+
 def checkValidPairNumber(pair_number):
     assert(pair_number>0)
     assert(pair_number<=25)
-
-result = print_color_map()
+    
+print_color_map()
+result = get_total_pair_numbers(major_colors, minor_colors)
 assert(result == 25)
 print("All is well (maybe!)\n")
